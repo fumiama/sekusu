1edc1e8bf5d1a4d496abee8ae6f9fdf06f0c9102
源文雨 <41315874+fumiama@users.noreply.github.com>
2024-02-22 03:24:41 +0900

    sync

--- components.d.ts
+++ components.d.ts
@@ -9,7 +9,6 @@ declare module 'vue' {
   export interface GlobalComponents {
     IconKantoku: typeof import('./src/components/icons/IconKantoku.vue')['default']
     VarButton: typeof import('@varlet/ui')['_ButtonComponent']
-    VarCard: typeof import('@varlet/ui')['_CardComponent']
     VarCell: typeof import('@varlet/ui')['_CellComponent']
     VarChip: typeof import('@varlet/ui')['_ChipComponent']
     VarCol: typeof import('@varlet/ui')['_ColComponent']
